08997af0c1b9eb1b00c68f99238cb11260a0dc3a
Bijit Mukherjee <bijit.9791@gmail.com>
2021-02-08 16:34:19 +0000

    Programs updated

--- least_sq_curve_fit.py
+++ least_sq_curve_fit.py
@@ -16,33 +16,28 @@ def model_pecs(R,param):
     Aexp(-beta1*R) - Bexp(-beta2*R) - Sum_{n=6,8}D_n*C_n/R^n
     D_n = 1 - exp(-beta1*R)Sum_{i=0}^n (beta1*R)^i/i! '''
 
-    param = param.reshape(nparam_pec, npec)
-    A = param[0,:]*10*Eh
-    B = param[1,:]*10*Eh
-    beta1 = param[2,:]/a0
-    beta2 = param[3,:]/a0
+    # param = param.reshape(nparam_pec, npec)
+    A = np.array([param[0], param[4], param[8], param[12]])*10*Eh
+    B = np.array([param[1], param[5], param[9], 0])*10*Eh
+    beta1 = np.array([param[2], param[6], param[10], param[13]])/a0
+    beta2 = np.array([param[3], param[7], param[11], 0])/a0
     c6pi, c6sig = 4067.6*Eh*a0**6, 4661.5*Eh*a0**6
-    C6 = np.array([c6sig,c6pi,c6pi,c6sig])
-    C8 = np.copy(C6)
-    C8 = C8*80*a0**2
+    c6 = np.array([c6sig,c6pi,c6pi,c6sig])
+    c8 = np.copy(c6)*80*a0**2
 
-    #repulsion term
-    V_rep = A*np.exp(-np.outer(R,beta1))
-    #attraction term
-    V_att = -B*np.exp(-np.outer(R,beta2))
-    V_att += -damp(R,beta1,6)*np.outer(1/R**6,C6) - damp(R,beta1,8)*np.outer(1/R**8,C8) 
+    v = A*np.exp(-np.outer(R,beta1)) - B*np.exp(-np.outer(R,beta2)) 
+    v += -damp(R,beta1,6)*np.outer(1/R**6,c6) - damp(R,beta1,8)*np.outer(1/R**8,c8)
 
-    V = V_rep + V_att
-    return V
+    return v
 
 def so_operator(R,param):
     '''a_SO model functions'''
 
-    param = param.reshape(nparam_aso, naso)
+    # param = param.reshape(nparam_aso, naso)
     a_so = 806.09 
-    r0 = param[0,:]
-    c = param[1,:]*100
-    alpha = param[2,:]
+    r0 = np.array([param[0], param[3], param[6], param[9]])
+    c = np.array([param[1], param[4], param[7], param[10]])*100
+    alpha = np.array([param[2], param[5], param[8], param[11]])
 
     a = a_so + c*(1-np.tanh(alpha*np.add.outer(R,-r0)))
     return a
@@ -51,7 +46,7 @@ def func_pecs(vmat,a_so):
     '''SO coupled PECs obtained from model
     spin-free PECs and a_SO functions'''
 
-    w_so = np.zeros([vmat.shape[0],9])
+    w_so = np.empty([vmat.shape[0],9])
     for i in range(vmat.shape[0]):
         a1, a2, a3, a4 = a_so[i,:]
         v1, v2, v3, v4 = vmat[i,:]
@@ -72,12 +67,10 @@ def func_pecs(vmat,a_so):
                 [0,0,np.sqrt(2*a3*a4/3),-np.sqrt(a1*a3/3),-a3]
                         ])
 
-        w1,_ = LA.eigh(u1 + u_so1)
-        w2,_ = LA.eigh(u2 + u_so2)
+        w_so[i,0] = v3+a3
+        w_so[i,1:4],_ = LA.eigh(u1 + u_so1)
+        w_so[i,4:],_ = LA.eigh(u2 + u_so2)
 
-        w = np.append(w1,w2)
-        w_so[i,:] = np.append([v3+a3],w)
-    
     return w_so
 
 
@@ -88,15 +81,15 @@ def func_residues(x,*args):
     data as args. x is the array of all parameters (PECs and a_SO).'''
 
     R_arr, v_so = args
-    param_pecs, param_aso = x[:npec*nparam_pec], x[npec*nparam_pec:]
+    param_pecs, param_aso = x[:nparam_pec], x[nparam_pec:]
     v = model_pecs(R_arr,param_pecs)         #calls model spin-free potentials
     a_so = so_operator(R_arr,param_aso)      #calls model a_SO functions
     w_so = func_pecs(v,a_so)               #calculates the 9 SO-coupled PECs
 
     # assigning weights
-    #lowering weights for any R<=3.5 ang
-    w_so[R_arr<=3.5,:] = 0.01*w_so[R_arr<=3.5,:]
-    v_so[R_arr<=3.5,:] = 0.01*v_so[R_arr<=3.5,:]
+    #lowering weights for any R<3.8 ang
+    w_so[R_arr<3.8,:] = 0.01*w_so[R_arr<3.8,:]
+    v_so[R_arr<3.8,:] = 0.01*v_so[R_arr<3.8,:]
     
     v_so = v_so.reshape(R_arr.size * 9)
     w_so = w_so.reshape(R_arr.size * 9)
@@ -109,12 +102,12 @@ def func_residues(x,*args):
 
 
 Eh, a0 = 2.1947463e5, 0.5291772         #Hartree to cm-1, bohr to ang
-npec, nparam_pec = 4, 4                 #No. of PECs, No. of parameters for each PEC
-naso, nparam_aso = 4, 3                 #No. of a_SO functions, parameters for each func
+nparam_pec = 14                         #No. of parameters for PECs
+nparam_aso = 12                         #No. of parameters for a_SO functions
 
 parameters = np.loadtxt("./fortran_pec_input.dat")
-param_pecs = parameters[:4,:]
-param_aso = parameters[4:,:]
+param_pecs = parameters[:nparam_pec]
+param_aso = parameters[nparam_pec:]
 
 #Guess parameters for spin-free PECs
 #Rows ----> parameters,  Columns ----> PECs
@@ -123,7 +116,7 @@ param_aso = parameters[4:,:]
 # 3rd row --> beta1 (in units of a0^-1)
 # 4th row --> beta2 (in units of a0^-1)
 
-param_pecs = param_pecs.reshape(param_pecs.size)
+# param_pecs = param_pecs.reshape(param_pecs.size)
 
 #Guess parameters for a_SO functions
 #Rows ----> parameters,  Columns ----> 4 functions
@@ -131,43 +124,46 @@ param_pecs = param_pecs.reshape(param_pecs.size)
 # 2nd row --> c (cm^-1)
 # 3rd row --> alpha (in units of ang^-1)
 
-param_aso = param_aso.reshape(param_aso.size)
+# param_aso = param_aso.reshape(param_aso.size)
 
 x0 = np.append(param_pecs,param_aso)
-ll = nparam_pec*npec
+# ll = nparam_pec*npec
 
-#Defining an array of uncertainties in the parameters
-A_sig, beta_sig = 0.001, 0.0001
-# A_sig, beta_sig = 0, 0
-c_sig, r0_sig, alpha_sig = 0.5, 0.05, 0.5
-x_sig = random.rand(x0.size)
-x_sig[0:8] = A_sig * (1-2*x_sig[0:8])
-x_sig[8:16] = beta_sig * (1-2*x_sig[8:16])
-x_sig[ll:ll+4] = r0_sig * (1-2*x_sig[ll:ll+4])
-x_sig[ll+4:ll+8] = c_sig * (1-2*x_sig[ll+4:ll+8])
-x_sig[ll+8:ll+12] = alpha_sig * (1-2*x_sig[ll+8:ll+12])
-x0 = x0 + x_sig
+# #Defining an array of uncertainties in the parameters
+# A_sig, beta_sig = 0.01, 0.001
+# # A_sig, beta_sig = 0, 0
+# c_sig, r0_sig, alpha_sig = 0.5, 0.05, 0.5
+# x_sig = random.rand(x0.size)
+# x_sig[0:8] = A_sig * (1-2*x_sig[0:8])
+# x_sig[8:16] = beta_sig * (1-2*x_sig[8:16])
+# x_sig[ll:ll+4] = r0_sig * (1-2*x_sig[ll:ll+4])
+# x_sig[ll+4:ll+8] = c_sig * (1-2*x_sig[ll+4:ll+8])
+# x_sig[ll+8:ll+12] = alpha_sig * (1-2*x_sig[ll+8:ll+12])
+# # x0 = x0 + x_sig
 
 #defining bounds on the parameters
 x_lbounds = np.zeros(x0.size)               #default lower bounds for all
 x_ubounds = np.inf * np.ones(x0.size)       #default upper bounds for all
-# x_lbounds[ll:ll+4] = 4                      #lower bounds for R0
-# x_lbounds[ll+4:ll+7] = -1                   #lower bounds for c
-x_lbounds[ll+4:ll+7] = -4.03045            #lower bounds for c
-x_lbounds[ll+7] = -0.5                           #lower bounds for c
-x_lbounds[ll+8:ll+12] = 0.5                     #lower bounds for alpha
-x_ubounds[7] = 1.0e-12                        #upper bound for B_^4Sigma
-x_ubounds[15] = 1.0e-12                        #upper bound for beta2_^4Sigma
-# x_ubounds[ll:ll+4] = 6                      #upper bounds for R0
-# x_ubounds[ll+3] = 1.e-12                   #upper bound for R0 of a4
-x_ubounds[ll+7] = 0.5                   #upper bound for c of a4
-# x_ubounds[ll+11] = 1.e-12                   #upper bound for alpha of a4
+x_lbounds[nparam_pec+1] = -4.03045
+x_lbounds[nparam_pec+4] = -4.03045
+x_lbounds[nparam_pec+7] = -4.03045
+x_lbounds[nparam_pec+10] = -0.2
+x_lbounds[nparam_pec] = 0.5
+x_lbounds[nparam_pec+3] = 0.5
+x_lbounds[nparam_pec+6] = 0.5
+x_lbounds[nparam_pec+9] = 0.5
+x_ubounds[nparam_pec] = 7
+x_ubounds[nparam_pec+3] = 7
+x_ubounds[nparam_pec+6] = 7
+x_ubounds[nparam_pec+9] = 7
+x_ubounds[nparam_pec+10] = 0.2
 
-for i in range(x0.size):
-    if x0[i] < x_lbounds[i]:
-        x0[i] = x_lbounds[i]
-    if x0[i] > x_ubounds[i]:
-        x0[i] = x_ubounds[i]
+
+# for i in range(x0.size):
+#     if x0[i] < x_lbounds[i]:
+#         x0[i] = x_lbounds[i]
+#     if x0[i] > x_ubounds[i]:
+#         x0[i] = x_ubounds[i]
 
 #Reading interpolated datafile
 abinitioFile = np.loadtxt("./PECs_so_fit_dense.dat")
@@ -192,20 +188,22 @@ print("Running optimization...\n")
 
 args = (R_arr, v_so)
 res = least_squares(func_residues,x0,bounds=(x_lbounds,x_ubounds),\
-                    ftol=1.e-7,xtol=1.e-8,gtol=1.e-8,args=args,verbose=1)
-param_pecs, param_aso = res.x[:npec*nparam_pec], res.x[npec*nparam_pec:]
+                    ftol=1.e-8,xtol=1.e-8,gtol=1.e-8,args=args,verbose=1)
+param_pecs, param_aso = res.x[:nparam_pec], res.x[nparam_pec:]
 
 print("Optimization done\n")
 
 param_wr = np.copy(res.x)
 
-txt1 = "{:18.6f}\t{:18.6f}\t{:18.6f}\t{:18.6f}\n\
-{:18.6f}\t{:18.6f}\t{:18.6f}\t{:18.6f}\n\
-{:18.6f}\t{:18.6f}\t{:18.6f}\t{:18.6f}\n\
-{:18.6f}\t{:18.6f}\t{:18.6f}\t{:18.6f}\n\n\
-{:18.6f}\t{:18.6f}\t{:18.6f}\t{:18.6f}\n\
-{:18.6f}\t{:18.6f}\t{:18.6f}\t{:18.6f}\n\
-{:18.6f}\t{:18.6f}\t{:18.6f}\t{:18.6f}\n".format(*param_wr)
+txt1 = "#Parameters for pecs\n"
+for i in range(nparam_pec):
+    txt1 += "{:12.6f}\n".format(param_wr[i])
+
+txt1 += "#Parameters for SO functions\n"
+for i in range(nparam_pec,param_wr.size):
+    txt1 += "{:12.6f}\n".format(param_wr[i])
+
+
 with open("output_param.dat","w") as f:
     f.write(txt1)
 
